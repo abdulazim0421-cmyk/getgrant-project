3a6be778194936955d2200f98b55776655969b9c
Azim1020 <abdulazim0421@gmail.com>
2026-06-14 15:43:06 +0600

    fix

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,25 +1,5 @@
-import type { Metadata } from "next";
-import "@/app/globals.css";
-import { Inter } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { redirect } from 'next/navigation';
 
-const inter = Inter({ subsets: ["latin", "cyrillic"], variable: "--font-sans" });
-
-export const metadata: Metadata = {
-    title: "GetGrant",
-    description: "Образование за рубежом и поиск грантов",
-};
-
-export function generateStaticParams() {
-    return [{ lang: "ru" }, { lang: "kg" }];
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html suppressHydrationWarning className={cn("font-sans", inter.variable)}>
-        <body className="antialiased bg-white" suppressHydrationWarning>
-        {children}
-        </body>
-        </html>
-    );
+export default function LangHome({ params }: { params: { lang: string } }) {
+    redirect(`/${params.lang}/Home`);
 }
